6cef846c191cdfd1d75d6adccccb840c0b875609
John Lee <ubwhole@yahoo.com>
2016-12-05 15:10:58 -0600

    Add Post to /notes.

--- server.js
+++ server.js
@@ -22,6 +22,21 @@ app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
       res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!"})
 });
  
+var bodyParser = require('body-parser');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+     
+app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+      if(!req.body.notes || typeof req.body.notes != "string") {
+              res.status(400).send("400 Bad Request")
+      }
+       
+      req.user.customData.notes = req.body.notes
+      req.user.customData.save()
+      res.status(200).end()
+})
+
 app.on('stormpath.ready',function(){
       console.log('Stormpath Ready');
 });
